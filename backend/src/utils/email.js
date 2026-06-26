c2ab5efaff68e679b30192e0200db0b0ed94e171
Omar khecharem <80176955+Omar-khecharem@users.noreply.github.com>
2026-06-26 15:23:01 +0100

    Update email.js

--- backend/src/utils/email.js
+++ backend/src/utils/email.js
@@ -5,7 +5,7 @@ const createTransporter = async () => {
     return nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: parseInt(process.env.EMAIL_PORT || '587'),
-      secure: process.env.EMAIL_SECURE === 'true',
+      secure: process.env.EMAIL_SECURE === 'false',
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
